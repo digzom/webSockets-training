b9437ed02d90ba1ea0d853f6b210f6aec0841918
digzom <dicksonmelo@gmail.com>
2022-02-23 08:58:17 -0300

    tá dando alguma treta com o objeto subscribers

--- real-time-chat/server/dist/index.js
+++ real-time-chat/server/dist/index.js
@@ -16,7 +16,13 @@ const typeDefs = `
   type Mutation {
     postMessage(user: String!, content: String!): ID!
   }
+
+  type Subscription {
+    messages: [Message!]
+  }
 `;
+const subscribers = [];
+const onMessagesUpdates = (fn) => subscribers.push(fn);
 const resolvers = {
     Query: {
         messages: () => messages,
@@ -29,10 +35,22 @@ const resolvers = {
                 user,
                 content,
             });
+            subscribers.forEach((fn) => fn());
             return id;
         },
     },
+    Subscription: {
+        messages: {
+            subscribe: (parent, args, { pubsub }) => {
+                const channel = Math.random().toString(36).slice(2, 15);
+                onMessagesUpdates(() => pubsub.publish(channel, { messages }));
+                setTimeout(() => () => pubsub.publish(channel, { messages }), 0);
+                return pubsub.asyncIteretor(channel);
+            },
+        },
+    },
 };
+const pubsub = new graphql_yoga_1.PubSub();
 const server = new graphql_yoga_1.GraphQLServer({ typeDefs, resolvers });
 server.start(({ port }) => {
     console.log(`Server running on http://localhost:${port}/`);
--- real-time-chat/server/index.ts
+++ real-time-chat/server/index.ts
@@ -1,4 +1,4 @@
-import { GraphQLServer } from "graphql-yoga"
+import { GraphQLServer, PubSub } from "graphql-yoga"
 
 const messages = []
 
@@ -16,7 +16,14 @@ const typeDefs = `
   type Mutation {
     postMessage(user: String!, content: String!): ID!
   }
+
+  type Subscription {
+    messages: [Message!]
+  }
 `
+
+const subscribers = []
+const onMessagesUpdates = (fn) => subscribers.push(fn)
 
 const resolvers = {
   Query: {
@@ -30,10 +37,22 @@ const resolvers = {
         user,
         content,
       })
+      subscribers.forEach((fn) => fn())
       return id
     },
   },
+  Subscription: {
+    messages: {
+      subscribe: (parent, args, { pubsub }) => {
+        const channel = Math.random().toString(36).slice(2, 15)
+        onMessagesUpdates(() => pubsub.publish(channel, { messages }))
+        setTimeout(() => () => pubsub.publish(channel, { messages }), 0)
+        return pubsub.asyncIteretor(channel)
+      },
+    },
+  },
 }
+const pubsub = new PubSub()
 
 const server = new GraphQLServer({ typeDefs, resolvers })
 
